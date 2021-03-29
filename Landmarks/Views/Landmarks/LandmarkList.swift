c9bd444c68bc9432cbb8c45eeacbc66a29391372
Pavel Chernyshev <titan100695@yandex.ru>
2021-03-29 21:25:56 +0300

    Handling User Input

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Павел Чернышев on 28.03.2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { (landmark) -> Bool in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
